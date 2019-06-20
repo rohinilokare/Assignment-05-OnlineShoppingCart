5d08eb8d0487b43e2af392448e7fa0e1c95f1ce0
rohinilokare <rohini@treeni.com>
2019-06-20 14:29:48 +0530

    feature::Ruby:Product Class ::created new function for select item

--- product.rb
+++ product.rb
@@ -14,26 +14,24 @@ class Product
 		end 
   end
 
-	def view_available_products
-			puts '******************Available Product List******************************* '
-		for data in @scanfile
-			puts data[0]+"  "+data[1]+"  "+data[2]+"  "+data[3]+"  "+data[4]
-		end
+  def view_available_products
+	puts '******************Available Product List******************************* '
+	for data in @scanfile
+		puts data[0]+"  "+data[1]+"  "+data[2]+"  "+data[3]+"  "+data[4]
 	end
+  end
 
+  def select_item
+       view_available_products
 
- 	
-	def select_item
-	  view_available_products
-
-		puts '***********Select Product which you want to add **************************'
-		puts 'choose product ID '
-		product_id = gets.to_i
-		puts 'Add no. of quantity '
-		quantity = gets.to_i
-		print quantity
-		CSV.open("file.csv", "a+") do |csv|
-			csv << [order_id,product_id,@nameHash["#{product_id}"],quantity,@priceHash["#{product_id}"]]
-		end
+	puts '***********Select Product which you want to add **************************'
+	puts 'choose product ID '
+	product_id = gets.to_i
+	puts 'Add no. of quantity '
+	quantity = gets.to_i
+	print quantity
+	CSV.open("file.csv", "a+") do |csv|
+		csv << [order_id,product_id,@nameHash["#{product_id}"],quantity,@priceHash["#{product_id}"]]
 	end
+  end
 end
