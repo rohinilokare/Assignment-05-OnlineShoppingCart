5ace7b5f3b79bd664802a86d01cec9ae7b563022
rohinilokare <rohini@treeni.com>
2019-06-21 07:50:12 +0530

    modified::Cart Class::as per new product class

--- cart.rb
+++ cart.rb
@@ -1,36 +1,26 @@
 require 'csv'
 class Cart
-	@@objects_created = 0
+
 	def initialize()
-  	@show_cart=CSV.read("file.csv")
+		@show_cart=CSV.read("cart1.csv")[1..-1]
 	end
+
 
 	def show_cart
+		puts 'PRODUCT_ID      PRODUCT_NAME      QUANTITY      PRICE/PRODUCT         '
 		for data in @show_cart
 			puts data[0]+"  "+data[1]+"  "+data[2]+"  "+data[3]
-		end
-	end
-
-	def remove_item(id)
-		puts id
-		for data in show_cart
-			puts data[0]
-			if data[0] == id
-				puts 'delete cart '
-			else
-				puts 'enter valid Product id'
-			end
 		end
 	end
 
 	def bill
 		@bill= 0.0
 		for data in @show_cart
-			id = data[0]
+			product_id = data[0]
 			x = Float(data[2])
 			y = Float(data[3])
 			@bill = x*y
-		print 'total bill for Product ID '+id+', is :'
+		print 'total bill for Product ID '+product_id+', is :'
 		puts @bill 
 		end
 	end
